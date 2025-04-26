e98cb49dc387a353dba429a885576a710ef6c91d
CedriMillare <cmamillare.student@ua.edu.ph>
2025-04-26 19:00:40 +0800

    initial commit

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,28 +1,36 @@
 export function TopNav() {
     return (
-            <nav className="sticky top-0 w-full flex items-center text-center justify-between p-4 bg-black text-white">
+            <nav className="sticky max-w flex items-center justify-between p-4 bg-black text-white">
                 <div className="text-xl font-bold">Lcarpio's Flower Shop</div>
                 
-                <div className="flex-1 flex justify-center">
+                <div className="flex gap-4 list-none">
                     <div className="relative group">
                         <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Home</a>
                     </div>
                     <div className="relative group">
                         <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Bouquet</a>
                         <ul className="fixed pt-5 hidden group-hover:block bg-white text-black rounded">
-                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Small Bouquet</a></li>
-                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Big Bouquet</a></li>
-                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Funeral</a></li>
-                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Wedding</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Simple Bouquet</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Premium Bouquet</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Special Bouquet</a></li>
+                        </ul>
+                    </div>
+                    <div className="relative group">
+                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">For Occasions</a>
+                        <ul className="fixed pt-5 hidden group-hover:block bg-white text-black rounded">
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Funeral Arrangement</a></li>
+                            <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Wedding Arrangement</a></li>
                         </ul>
                     </div>
                     <div className="relative group">
                         <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Flowers</a>
-                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Orders</a>
+                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Cart</a>
                     </div>
                 </div>
-                <div className="w-32"></div>
-
+                <div>
+                    <button className=" text-white px-4 py-2 rounded hover:bg-gray-500">Sign In</button>
+                    <button className=" text-white px-4 py-2 rounded hover:bg-gray-500">Sign Up</button>
+                </div>
             </nav>
 
     );
