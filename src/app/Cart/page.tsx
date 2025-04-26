92ff6d3e4043aef8b106e8828119891f47e40783
CedriMillare <cmamillare.student@ua.edu.ph>
2025-04-26 21:11:48 +0800

    initial commit

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -5,7 +5,7 @@ export default function CartPage() {
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#454446] to-[#1d1d22] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Cart
+         Cart
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
