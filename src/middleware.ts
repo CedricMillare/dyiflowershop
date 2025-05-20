3b64d19281bbe99681f5a400ad2979b78b1242c7
CedriMillare <cmamillare.student@ua.edu.ph>
2025-05-20 18:02:23 +0800

    initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const isProtectedRoute = createRouteMatcher(["/dashboard/(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/dashboard/(.*)"]);
 
-// export default clerkMiddleware((auth, request)=>{
-//     if (isProtectedRoute(request)) auth.protect();
-//     });
+export default clerkMiddleware((auth, request)=>{
+    if (isProtectedRoute(request)) auth.protect();
+    });
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 
 export const config = {
   matcher: [
